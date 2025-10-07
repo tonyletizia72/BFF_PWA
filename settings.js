2dc83547724e9128215f4018b43fcfdf5cee5d71
tonyletizia72 <tonyletizia72@gmail.com>
2025-10-07 14:42:40 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 const SETTINGS = {
-  WEBHOOK_URL: "https://script.google.com/macros/s/AKfycbwJ5Y3OpGFoC3TTjo55I0PY3Ssb4rk_5goTV17QLwTxEdouXE01DuB2coFzlkyXe8ot0Q/exec",
+  WEBHOOK_URL: "https://script.google.com/macros/s/AKfycbymx2rM7NKcwvp52q4-NzweWGLoP4ZZRBME2JfSPahVZGaYEyw4J-NMtcHJxgptwBKQ7Q/exec",
   SECRET: "BFF"
 };
+
 
