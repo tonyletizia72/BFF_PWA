43de183a29bc9f0b7ddec2b8157d2aa9c8dca153
tonyletizia72 <tonyletizia72@gmail.com>
2025-10-07 21:41:37 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,11 @@
 // settings.js — Boxing for Fitness (Production)
 window.SETTINGS = {
-  // Your new Apps Script Web App URL (public /exec URL)
+  // Webhook for your live Apps Script deployment (public /exec URL)
   WEBHOOK_URL:
-    https://script.google.com/macros/s/AKfycbyI-9BNZT-FCeE8vq2eIHsnqvfdfepUkLWoP1Yw0qUbLW-2_6XmyTRPiaqE2PlU41-wJA/exec,
+    "https://script.google.com/macros/s/AKfycbxd7Do-Rqa_Lfp4LZNQlUZRqCVn2hOHTygm87HNkds5BSZw9953s-2OQV7hnHumZfIXGQ/exec",
 
-  // Must match the SECRET in Code.gs
+  // Must match SECRET in your Code.gs
   SECRET: "BFF"
 };
 
 console.log("[BFF] SETTINGS loaded:", window.SETTINGS.WEBHOOK_URL);
-
