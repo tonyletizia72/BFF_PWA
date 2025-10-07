c3c7626594d4374880cc4c3c194114f8ae9f77b9
tonyletizia72 <tonyletizia72@gmail.com>
2025-10-07 21:20:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
   if (typeof window.SETTINGS === "undefined") {
     window.SETTINGS = {
       WEBHOOK_URL:
-        "https://script.google.com/macros/s/AKfycbzth19M5OuTnruzpv_5dRFSt_4UfjleReZm3N5VuSqWw-6EvZgbhu4wA3r91DsmHNW9/exec",
+        https://script.google.com/macros/s/AKfycbyI-9BNZT-FCeE8vq2eIHsnqvfdfepUkLWoP1Yw0qUbLW-2_6XmyTRPiaqE2PlU41-wJA/exec,
       SECRET: "BFF"
     };
     console.warn("[BFF] settings.js not found; using embedded fallback SETTINGS.");
